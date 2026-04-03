6e6f6aa2af62fdd386da77f600399077ca4da8b2
mikhaeilryad35-web <mikhaeilryad35@gmail.com>
2026-04-03 23:35:44 +0200

    Exercise 1:

--- Program.cs
+++ Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Exercise 1:
+            //List<int> grades = new List<int> { 85, 92, 78, 95, 88, 70, 100, 65 };
+            //Console.WriteLine("Grades: " + string.Join(", ", grades));
+            //Console.WriteLine("Count: " + grades.Count);
+            //Console.WriteLine("first: " + grades.Max());
+            //Console.WriteLine("last: " + grades.Min());
+            //grades.Sort();
+            //Console.WriteLine("sort: " + string.Join(", ", grades));
+            //foreach (var item in grades)
+            //{
+            //   if(item>90)
+            //        Console.WriteLine("first grades : " + string.Join(", ", item));
+            //}
+            //foreach (var item in grades)
+            //{
+            //    if (item < 75) {
+            //        Console.WriteLine("failing grades : " + string.Join(", ", item));
+            //    }
+                
+            //}
+            //foreach (var item in grades)
+            //{
+            //    if (item < 75)
+            //    {
+            //        Console.WriteLine("After Remove:" + string.Join(", ",  grades.RemoveAll));
+            //    }
+
+            //}
+            //Console.WriteLine("Contains 100: " + grades.Contains(100));
+            #endregion
         }
     }
 }
