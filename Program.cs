871573c378ae4bacaae8a6171245511a2bd61b3f
mikhaeilryad35-web <mikhaeilryad35@gmail.com>
2026-04-03 23:39:59 +0200

    Exercise 3:

--- Program.cs
+++ Program.cs
@@ -44,28 +44,69 @@
             //        {350, "Mona"}
             //    };
 
-            
+
             //foreach (var item in leaderboard)
             //    Console.WriteLine($"{item.Key} = {item.Value}");
-             
+
             //Console.WriteLine("First Key: " + leaderboard.First().Key);
             //Console.WriteLine("First Value: " + leaderboard.First().Value);
 
-            
+
             //Console.WriteLine("Contains 500: " + leaderboard.ContainsKey(500));
 
-             
+
             //if (leaderboard.TryGetValue(999, out string player))
             //    Console.WriteLine(player);
             //else
             //    Console.WriteLine("Score 999 not found");
 
-             
+
             //leaderboard.Remove(200);
 
             //Console.WriteLine("After remove:");
             //foreach (var item in leaderboard)
             //    Console.WriteLine($"{item.Key} = {item.Value}");
+            #endregion
+
+            #region Exercise 3:
+            //  Dictionary<string, string> phoneBook = new Dictionary<string, string>()
+            //{
+            //    {"Ahmed", "010"},
+            //    {"Sara", "011"},
+            //    {"Ali", "012"},
+            //    {"Mona", "015"}
+            //};
+
+             
+            //phoneBook["John"] = "016";
+
+             
+            //try
+            //{
+            //    phoneBook.Add("Ahmed", "999");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Error: " + ex.Message);
+            //}
+
+             
+            //bool added = phoneBook.TryAdd("Ahmed", "999");
+            //Console.WriteLine("TryAdd success: " + added);
+
+            
+            //if (!phoneBook.ContainsKey("Khaled"))
+            //    Console.WriteLine("Not found");
+
+             
+            //string value = phoneBook.GetValueOrDefault("Khaled", "Not Found");
+            //Console.WriteLine(value);
+
+           
+            //Console.WriteLine(string.Join(", ", phoneBook.Keys));
+
+            
+            //Console.WriteLine(string.Join(", ", phoneBook.Values));
             #endregion
         }
     }
