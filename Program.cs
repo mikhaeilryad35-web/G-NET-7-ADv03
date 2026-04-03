a0fa9fca843186c77ae12314c8c789656d62c89f
mikhaeilryad35-web <mikhaeilryad35@gmail.com>
2026-04-03 23:38:03 +0200

    Exercise 2:

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@
             //    if (item < 75) {
             //        Console.WriteLine("failing grades : " + string.Join(", ", item));
             //    }
-                
+
             //}
             //foreach (var item in grades)
             //{
@@ -33,6 +33,39 @@
 
             //}
             //Console.WriteLine("Contains 100: " + grades.Contains(100));
+            #endregion
+
+            #region Exercise 2:
+            //       SortedDictionary<int, string> leaderboard = new SortedDictionary<int, string>()
+            //    {
+            //        {500, "Ahmed"},
+            //        {200, "Sara"},
+            //        {800, "Ali"},
+            //        {350, "Mona"}
+            //    };
+
+            
+            //foreach (var item in leaderboard)
+            //    Console.WriteLine($"{item.Key} = {item.Value}");
+             
+            //Console.WriteLine("First Key: " + leaderboard.First().Key);
+            //Console.WriteLine("First Value: " + leaderboard.First().Value);
+
+            
+            //Console.WriteLine("Contains 500: " + leaderboard.ContainsKey(500));
+
+             
+            //if (leaderboard.TryGetValue(999, out string player))
+            //    Console.WriteLine(player);
+            //else
+            //    Console.WriteLine("Score 999 not found");
+
+             
+            //leaderboard.Remove(200);
+
+            //Console.WriteLine("After remove:");
+            //foreach (var item in leaderboard)
+            //    Console.WriteLine($"{item.Key} = {item.Value}");
             #endregion
         }
     }
