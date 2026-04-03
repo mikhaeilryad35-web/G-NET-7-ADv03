c3df22499df96213dd11494225e5cfd7f7109dfd
mikhaeilryad35-web <mikhaeilryad35@gmail.com>
2026-04-03 23:41:24 +0200

    Exercise 4:

--- Program.cs
+++ Program.cs
@@ -77,10 +77,10 @@
             //    {"Mona", "015"}
             //};
 
-             
+
             //phoneBook["John"] = "016";
 
-             
+
             //try
             //{
             //    phoneBook.Add("Ahmed", "999");
@@ -90,23 +90,58 @@
             //    Console.WriteLine("Error: " + ex.Message);
             //}
 
-             
+
             //bool added = phoneBook.TryAdd("Ahmed", "999");
             //Console.WriteLine("TryAdd success: " + added);
 
-            
+
             //if (!phoneBook.ContainsKey("Khaled"))
             //    Console.WriteLine("Not found");
 
-             
+
             //string value = phoneBook.GetValueOrDefault("Khaled", "Not Found");
             //Console.WriteLine(value);
 
-           
+
             //Console.WriteLine(string.Join(", ", phoneBook.Keys));
 
-            
+
             //Console.WriteLine(string.Join(", ", phoneBook.Values));
+            #endregion
+
+            #region Exercise 4:
+            //      HashSet<string> emails = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+            //    {
+            //        "ahmed@test.com",
+            //        "AHMED@test.com",
+            //        "sara@test.com",
+            //        "Sara@Test.Com"
+            //    };
+
+            //Console.WriteLine("Count: " + emails.Count);
+            
+
+             
+            //HashSet<int> A = new HashSet<int> { 1, 2, 3, 4, 5 };
+            //HashSet<int> B = new HashSet<int> { 4, 5, 6, 7, 8 };
+
+             
+            //var union = new HashSet<int>(A);
+            //union.UnionWith(B);
+            //Console.WriteLine("Union: " + string.Join(", ", union));
+
+            
+            //var intersect = new HashSet<int>(A);
+            //intersect.IntersectWith(B);
+            //Console.WriteLine("Intersect: " + string.Join(", ", intersect));
+
+            
+            //var except = new HashSet<int>(A);
+            //except.ExceptWith(B);
+            //Console.WriteLine("Except: " + string.Join(", ", except));
+
+            
+            //Console.WriteLine(new HashSet<int> { 1, 2 }.IsSubsetOf(A));
             #endregion
         }
     }
